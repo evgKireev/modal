d611c7650942fba7b249639960a9796b33cda686
evgKireev <bob1_k@mail.ru>
2022-10-12 22:27:35 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Modal from './components/Modal/Modal'
+
+function App() {
+  return (
+    <div className="App">
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
